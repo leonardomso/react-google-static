59416871a1c8e50903d8ffab024a1d084c132c8d
leonardomso <leonardomso11@gmail.com>
2020-01-14 12:53:02 -0300

    More changes and example

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,22 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
 
-const App = () => {
-  return (
-    <div>
-      <Thing />
-    </div>
-  );
-};
+import GoogleStaticMap from '../src'
+
+const App = () => (
+  <div>
+    <h1>Google Static Maps API</h1>
+
+    <GoogleStaticMap
+      style={{ width: 200, height: 200 }}
+      latitude={32.064171}
+      longitude={34.7748068}
+      size={{ width: 300, height: 550 }}
+      zoom={13}
+      apiKey="testing"
+    />
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
